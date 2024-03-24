d453786e88e5df523d991787bc523219d75c9e97
Carlos Iván Montiers Aguilera <cmontiers@gmail.com>
2024-03-24 14:34:39 -0300

    Remove file write operation for total_node_count

--- stats.c
+++ stats.c
@@ -33,19 +33,9 @@ void InitNodeCount()
 }
 
 void IncNodeCount(int dth) {
-
-	FILE *fp;
-
 	total_node_count++;
 	IdaInfo->node_count++;
 	IdaInfo->nodes_depth[dth]++;
-	if (total_node_count%100000==0) {
-		fp = fopen("nodecount","w");
-		if (fp != NULL) {
-			fprintf(fp,"%li",total_node_count);
-			fclose(fp);
-		}
-	}
 }
 
 void init_opts() {
